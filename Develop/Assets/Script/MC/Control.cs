16dde19caae8fecb35c30a74774555e1ef3b9e8b
Rohaid <rohaidb1999@gmail.com>
2022-03-28 14:15:37 -0400

    Added Polygon Farm Pack + Movement + Rotation

---
+++ Develop/Assets/Script/MC/Control.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Script/MC/Control.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Control : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Control()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Control"",
+    ""maps"": [
+        {
+            ""name"": ""Movement"",
+            ""id"": ""8db47a48-5006-4fcb-8082-2aefbaa6706c"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""80cfd5da-3583-46ec-b2e5-3804fc08a998"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""View"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""373658f2-0ee0-433b-9308-4b5d221c37cb"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""a438e0cb-3de3-437b-8bdf-297cd2bc54d7"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2e417054-b759-4971-8db7-e1dbf42ab43d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""ad2a201a-4d16-4787-8dd2-2d2a0ecfae2d"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""7dafa19c-7039-4a88-b9e6-1d76bf65aa50"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c857dded-46fb-4cf1-b197-13ae6fcab031"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f9cf6b56-6c8f-4f21-addd-6da4f9a84a23"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""View"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Movement
+        m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
+        m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
+        m_Movement_View = m_Movement.FindAction("View", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movement
+    private readonly InputActionMap m_Movement;
+    private IMovementActions m_MovementActionsCallbackInterface;
+    private readonly InputAction m_Movement_Move;
+    private readonly InputAction m_Movement_View;
+    public struct MovementActions
+    {
+        private @Control m_Wrapper;
+        public MovementActions(@Control wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Movement_Move;
+        public InputAction @View => m_Wrapper.m_Movement_View;
+        public InputActionMap Get() { return m_Wrapper.m_Movement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementActions instance)
+        {
+            if (m_Wrapper.m_MovementActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @View.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnView;
+                @View.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnView;
+                @View.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnView;
+            }
+            m_Wrapper.m_MovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @View.started += instance.OnView;
+                @View.performed += instance.OnView;
+                @View.canceled += instance.OnView;
+            }
+        }
+    }
+    public MovementActions @Movement => new MovementActions(this);
+    public interface IMovementActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnView(InputAction.CallbackContext context);
+    }
+}
---
+++ Develop/Assets/Script/MC/Movement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] Control _playerInput;
+    [SerializeField] Rigidbody _rigidBody;
+
+    protected Vector2 input_view;
+    protected Vector2 input_move;
+    private Vector3 move;
+    private Vector3 direction;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] protected float movementSmoothing = 0.1f;
+    private float yaw = 0f;
+
+    [Header("Audio")] // To be added
+    private AudioClip _walking;
+    private AudioClip _pickup;
+
+
+    void Awake()
+    {
+        _playerInput = new Control();
+        _rigidBody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable(){
+        _playerInput.Movement.Enable();
+        _playerInput.Movement.View.performed += x => input_view = x.ReadValue<Vector2>().normalized;
+        _playerInput.Movement.View.canceled += x => input_view = x.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnDisable(){
+        _playerInput.Movement.Disable();
+        _playerInput.Movement.View.Disable();
+    }
+    
+    void Update()
+    {
+        RotateMove();
+    }
+
+    void FixedUpdate()
+    {
+        //add if (paused) here
+        Move();
+    }
+
+    private void Move(){
+        input_move = _playerInput.Movement.Move.ReadValue<Vector2>();
+        direction = (transform.rotation * Vector3.forward * input_move.y + transform.rotation * Vector3.right *input_move.x).normalized;
+        move = direction * speed;
+         Vector3 targetVelocity = new Vector3(move.x * Time.fixedDeltaTime * speed, _rigidBody.velocity.y, move.z * speed * Time.fixedDeltaTime);
+        _rigidBody.velocity = Vector3.SmoothDamp(_rigidBody.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+    }
+
+    private void RotateMove(){
+        yaw = input_view.normalized.x * Time.deltaTime * cameraSpeed;
+        transform.Rotate(Vector3.up * yaw);
+    }
+}
